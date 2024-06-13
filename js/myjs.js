ea68690567b45c071f586c6ff876d8575eced04b
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-13 13:34:09 +0200

    add: event to container for play

--- js/myjs.js
+++ js/myjs.js
@@ -3,7 +3,19 @@ let contSquare = document.querySelector('.container');
 let numSquare = 100;
 
 for (let i = 1; i <= numSquare; i++) {
-    contSquare.innerHTML += `<div class="square">${i}</div>`
+    contSquare.innerHTML += `<div class="square">${i}</div>`;
 }
+
+contSquare.addEventListener('click', (event) =>{
+    // rintraccio l'elemento e lo manipolo
+    let element = event.target.classList
+    if(element.value === 'square'){
+        element.add('clicked')
+    }else{
+        element.remove('clicked')
+    }
+    // stampo in console il numero che è stato cliccato
+    console.log(event.target.textContent)
+});
 
     
