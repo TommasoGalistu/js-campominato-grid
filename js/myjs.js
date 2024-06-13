4b9dc88a773276511fee709de2106dc06d1f0981
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-13 13:24:42 +0200

    add: element dynamically in page

---
+++ js/myjs.js
@@ -0,0 +1,9 @@
+let contSquare = document.querySelector('.container');
+
+let numSquare = 100;
+
+for (let i = 1; i <= numSquare; i++) {
+    contSquare.innerHTML += `<div class="square">${i}</div>`
+}
+
+    
